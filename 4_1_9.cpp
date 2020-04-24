2521d80355168cd967930f56176889916aa612b8
NIkitaYushakov <64080940+NIkitaYushakov@users.noreply.github.com>
2020-04-24 12:23:58 +0300

    Add files via upload

---
+++ 4_1_9.cpp
@@ -0,0 +1,30 @@
+/* Этот код просто напоминание как выглядит класс Foo и функция foo_says.
+ * Не нужно его расскоментировать и/или менять.
+ *
+ * #include <iostream>
+ *
+ * struct Foo {
+ *     void say() const { std::cout << "Foo says: " << msg << "\n"; }
+ * protected:
+ *     Foo(const char *msg) : msg(msg) { }
+ * private:
+ *     const char *msg;
+ * };
+ *
+ * void foo_says(const Foo &foo) { foo.say(); }
+ *
+ */
+
+// Вам нужно определить функцию get_foo, как описано в задании,
+// чтобы компилировался и работал как ожидается следующий код:
+//
+// foo_says(get_foo("Hello!"));
+
+struct I_will_get_you : Foo {
+    I_will_get_you(const char *msg) : Foo(msg)
+    {}
+};
+struct I_will_get_you get_foo(const char *msg) {
+    I_will_get_you a(msg);
+    return a;
+}
