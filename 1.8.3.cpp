d6734a0d5300815577275a904b1029dd0536fd79
kek4jk <64089144+kek4jk@users.noreply.github.com>
2020-04-25 12:57:18 +0300

    Update 1.8.3.cpp

--- 1.8.3.cpp
+++ 1.8.3.cpp
@@ -1,1 +1,1 @@
-#define MAX(x, y, z) {int x_ = (x); int y_ = (y); int c_ = (x_ >= y_ ? c_ = x_ : c_ = y_); z = c_;}
+#define MAX(x, y, r)/* присваивает r максимум из x и y */{ int x_ = (x),y_ = (y), c_ = (x_ >= y_ ?  x_ :  y_); r = c_;}
