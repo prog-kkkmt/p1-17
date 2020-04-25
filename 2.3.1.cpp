7fdd665dc31c6defaf49bbd24fa9e194163b4e1a
kek4jk <64089144+kek4jk@users.noreply.github.com>
2020-04-25 19:14:21 +0300

    Update 2.3.1.cpp

--- 2.3.1.cpp
+++ 2.3.1.cpp
@@ -1,25 +1,13 @@
-void swap(int &a, int &b)
-{
-    int k = a;
-    a = b;
-    b = k;
-}
-
-void reverse(int* a, unsigned size)
-{
-    for (int i = 0; i < size/2; ++i)
-    {
-        swap(a[i],a[size-1-i]);
+void reverse(int a[], unsigned size){
+    int temp = a[0];
+    for(int i = 0; i < size - 1; i++){
+        a[i] = a[i+1];
     }
+    a[size-1] = temp;
 }
-
-void rotate(int a[], unsigned size, int n)
-{
-    if (n > size)
-    {
-        n %= size;
+void rotate(int a[], unsigned size, int shift){
+    shift %= size; // cдвиг
+    for(int i = 0; i < shift; i++){
+        reverse(a, size);
     }
-    reverse(a, n);
-    reverse(a + n, size - n);
-    reverse(a, size);
 }
