10e5b617d5585dedf72e91a1ac0425e4522222e0
alex1111233 <44643637+alex1111233@users.noreply.github.com>
2020-04-25 13:55:03 +0300

    Add files via upload

---
+++ 1.8.10..c
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    int a, b;
+
+ cin >> T;
+    for( int i = 0; i < T; i++)
+    {
+ cin >> a >> b;
+ cout << a + b << endl;
+    }
+    return 0;
+}
