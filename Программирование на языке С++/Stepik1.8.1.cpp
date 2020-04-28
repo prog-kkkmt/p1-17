d6daa881b419fc448eb04d583814c98f23c899f8
MariaForest <64083452+MariaForest@users.noreply.github.com>
2020-04-28 15:25:21 +0300

    Rename 1.8.1Stepik.cpp to Программирование на языке С++/Stepik1.8.1.cpp

---
+++ Программирование на языке С++/Stepik1.8.1.cpp
@@ -0,0 +1,12 @@
+// îïðåäåëèòå òîëüêî ôóíêöèþ power, ãäå
+//    x - ÷èñëî, êîòîðîå íóæíî âîçâåñòè â ñòåïåíü
+//    p - ñòåïåíü, â êîòîðóþ íóæíî âîçâåñòè x
+//
+
+int power(int x, unsigned p) {
+int answer = 1;
+for (int i = 0; i < p; ++i) {
+    answer *= x;
+    }
+    return answer;
+}
