cbb824fcb1a988364957c5e9ecd72877354f66b5
OVOSKOP <parodia1234321@gmail.com>
2020-04-28 12:29:53 +0300

    New tree

--- 2.4.6.cpp
+++
@@ -1,8 +0,0 @@
-// Напишите функцию, которая вычисляет длину C-style строки.
-
-unsigned strlen(const char *str)
-{
-  unsigned i = 0;
-  while(*str++) i++;
-  return i;
-}
--- 2.4.7.cpp
+++
@@ -1,8 +0,0 @@
-// Вам требуется реализовать функцию конкатенации (склейки) двух C-style строк.
-
-void strcat(char *to, const char *from)
-{
-    while(*to) to++;
-    while(*to++ = *from++);
-    *to++ = '\0';
-}
