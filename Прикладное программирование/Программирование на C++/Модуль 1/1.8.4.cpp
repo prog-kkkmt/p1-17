6d8810f947a076ccdda190e820bc15f8bc30534f
OVOSKOP <40697173+OVOSKOP@users.noreply.github.com>
2020-04-30 11:37:01 +0300

    Update 1.8.4.cpp

--- Прикладное программирование/Программирование на C++/Модуль 1/1.8.4.cpp
+++ Прикладное программирование/Программирование на C++/Модуль 1/1.8.4.cpp
@@ -10,7 +10,11 @@
 //    x - число, которое нужно возвести в степень
 //    p - степень, в которую нужно возвести x
 //
-
+//Выполнил: Пищулин Игорь
+//Источник: Stepik Computer Science Center (CS центр)
+//Программирование на языке C++
+//1.8.4 https://stepik.org/lesson/13377/step/4
+//25.04.2020
 int power(int x, unsigned p) {
     int answer = 1;
     for (int i = 0; i < p; i++)
