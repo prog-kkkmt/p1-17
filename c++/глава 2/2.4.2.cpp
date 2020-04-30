575fd561d7df33f1e76940792e6d6fb8ea059307
fesaym <64082384+fesaym@users.noreply.github.com>
2020-04-30 12:31:53 +0300

    Update 2.4.2.cpp

--- c++/глава 2/2.4.2.cpp
+++ c++/глава 2/2.4.2.cpp
@@ -1,3 +1,6 @@
+/*Вам требуется реализовать функцию конкатенации (склейки) двух C-style строк. */
+/*Величко П1-17*/
+/*https://stepik.org/lesson/540/step/7?auth=registration&unit=863*/
 void strcat(char *to, const char *from)
 {
     while (*to)
