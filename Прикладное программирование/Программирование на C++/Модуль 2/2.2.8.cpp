bc63582fa79b41fbd3288c1aa1a66ab378040280
OVOSKOP <40697173+OVOSKOP@users.noreply.github.com>
2020-04-30 11:40:24 +0300

    Update 2.2.8.cpp

--- Прикладное программирование/Программирование на C++/Модуль 2/2.2.8.cpp
+++ Прикладное программирование/Программирование на C++/Модуль 2/2.2.8.cpp
@@ -1,6 +1,10 @@
 // Напишите рекурсивную функцию вычисления наибольшего 
 // общего делителя двух положительных целых чисел (Greatest Common Divisor, GCD).
-
+//Выполнил: Пищулин Игорь
+//Источник: Stepik Computer Science Center (CS центр)
+//Программирование на языке C++
+//2.2.8 https://stepik.org/lesson/538/step/8
+//25.04.2020
 unsigned gcd(unsigned a, unsigned b)
 {
     if (a < b)
