1c6c839bdcf128f9c9471ad43a04746c31f8db78
OVOSKOP <40697173+OVOSKOP@users.noreply.github.com>
2020-04-30 11:42:15 +0300

    Update 2.4.6.cpp

--- Прикладное программирование/Программирование на C++/Модуль 2/2.4.6.cpp
+++ Прикладное программирование/Программирование на C++/Модуль 2/2.4.6.cpp
@@ -1,5 +1,9 @@
 // Напишите функцию, которая вычисляет длину C-style строки.
-
+//Выполнил: Пищулин Игорь
+//Источник: Stepik Computer Science Center (CS центр)
+//Программирование на языке C++
+//2.4.6 https://stepik.org/lesson/540/step/6
+//25.04.2020
 unsigned strlen(const char *str)
 {
   unsigned i = 0;
