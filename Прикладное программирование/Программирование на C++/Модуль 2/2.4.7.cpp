6ab822a38bc42b3b9fd226fca3c20fd7c7daf8ac
OVOSKOP <40697173+OVOSKOP@users.noreply.github.com>
2020-04-30 11:42:54 +0300

    Update 2.4.7.cpp

--- Прикладное программирование/Программирование на C++/Модуль 2/2.4.7.cpp
+++ Прикладное программирование/Программирование на C++/Модуль 2/2.4.7.cpp
@@ -1,5 +1,9 @@
 // Вам требуется реализовать функцию конкатенации (склейки) двух C-style строк.
-
+//Выполнил: Пищулин Игорь
+//Источник: Stepik Computer Science Center (CS центр)
+//Программирование на языке C++
+//2.4.7 https://stepik.org/lesson/540/step/7
+//25.04.2020
 void strcat(char *to, const char *from)
 {
     while(*to) to++;
