e283a19d653b99442fada45e7074f0040f518d03
zvuki_dot_neta <33006217+DanyaGiker@users.noreply.github.com>
2020-04-30 18:06:00 +0300

    Update st221.cpp

--- stepik_cpp/ЛЗ_3/st221.cpp
+++ stepik_cpp/ЛЗ_3/st221.cpp
@@ -1,3 +1,11 @@
+/*Напишите рекурсивную функцию вычисления наибольшего общего делителя двух положительных целых чисел (Greatest Common Divisor, GCD). 
+Для этого воспользуйтесь следующими свойствами:
+
+GCD(a, b) = GCD(b, a mod b)
+GCD(a, b) = GCD(b,a mod b)
+GCD(0, a) = GCD(0,a)=a
+GCD(a, b) = GCD(b, a)GCD(a,b)=GCD(b,a)
+*/
 //https://stepik.org/lesson/538/step/8?unit=861
 //Фатеев Д.И.
 
