e3e35066b434768ff263518515c43d9f66daeba3
OVOSKOP <40697173+OVOSKOP@users.noreply.github.com>
2020-04-30 11:59:57 +0300

    Update 4.4.7.cpp

--- Прикладное программирование/Программирование на C++/Модуль 4/4.4.7.cpp
+++ Прикладное программирование/Программирование на C++/Модуль 4/4.4.7.cpp
@@ -1,7 +1,11 @@
 // Вам требуется реализовать функцию, которая принимает на вход два указателя на базовый класс 
 // Expression, и возвращает true, если оба указателя указывают на самом деле на объекты одного 
 // и того же класса, и false в противном случае
-
+//Выполнил: Пищулин Игорь
+//Источник: Stepik Computer Science Center (CS центр)
+//Программирование на языке C++
+//https://stepik.org/lesson/555/step/7
+//26.04.2020
 bool check_equals(Expression const *left, Expression const *right)
 {
     return *((int*)left) == *((int*)right);
