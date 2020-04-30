04adc0379775d48f16386a0bccb624b45c074ea4
fesaym <64082384+fesaym@users.noreply.github.com>
2020-04-30 12:24:58 +0300

    Update 2,2,1.cpp

--- c++/глава 2/2,2,1.cpp
+++ c++/глава 2/2,2,1.cpp
@@ -1,3 +1,6 @@
+/*Напишите рекурсивную функцию вычисления наибольшего общего делителя двух положительных целых чисел (Greatest Common Divisor, GCD).*/
+/*Величко П1-17*/
+/*https://stepik.org/lesson/538/step/8?unit=861*/
 unsigned gcd(unsigned a, unsigned b)
 {
         if (b == 0)
