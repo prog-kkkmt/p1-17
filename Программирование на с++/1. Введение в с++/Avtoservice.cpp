28595d1d798b0bcf10e3441699298d5e2a89b7ba
alex1111233 <44643637+alex1111233@users.noreply.github.com>
2020-04-30 11:33:02 +0300

    Add files via upload

---
+++ Программирование на с++/1. Введение в с++/Avtoservice.cpp
@@ -0,0 +1,74 @@
+/*
+Выполнил Бобнев Алексей П1-17
+Задача: написать программу для автосервиса.
+*/
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+struct Data ///Структура
+{
+    int day, month, year, money;
+}d[100];
+
+struct People
+{
+    string name, fam;
+    int number_phone;
+}p[100];
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "Автосервис V 0.0.1." << endl; ///Название программы
+
+    int menu; ///Меню
+    cout << "1 - Выручка" << endl;
+    cout << "2 - Сотрудники" << endl;
+    cout << "0 - Выход" << endl;
+    cin >> menu;
+
+    while ((menu == 1) || (menu == 2))
+    {
+        if (menu == 1)
+        {
+            int n; ///Количество записей
+            cout << "Введите количество записей:" << endl;
+            cin >> n;
+            cout << "Введите день, месяц, год и выручку:" << endl;
+            int i; ///Переменная для for
+            for (i = 0; i < n; i++) cin >> d[i].day >> d[i].month >> d[i].year >> d[i].money;
+            for (i = 0; i < n; i++)
+            {
+                cout << d[i].day << "." << d[i].month
+                << "." << d[i].year << " " << d[i].money << " руб." << endl;
+            }
+        }
+
+        if (menu == 2)
+        {
+            int n, i;
+            cout << "Введите количество записей:" << endl;
+            cin >> n;
+            cout << "Введите имя, фамилия и номер телефона (Формат 89165555555):" << endl;
+            for (i = 0; i < n; i++)
+            {
+                cin >> p[i].name >> p[i].fam >> p[i].number_phone;
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                cout << p[i].name << " " << p[i].fam << " " << p[i].number_phone << endl;
+            }
+        }
+
+        cout << "1 - Выручка" << endl;
+        cout << "2 - Сотрудники" << endl;
+        cout << "0 - Выход" << endl;
+        cin >> menu;
+    }
+    return 0;
+}
