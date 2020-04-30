f7d5e8c393558fb6a583faee7481244c801f3e20
kovaleevaaaa <64081026+kovaleevaaaa@users.noreply.github.com>
2020-04-30 15:16:00 +0300

    Add files via upload

---
+++ C++ stepik/2.6.10.cpp
@@ -0,0 +1,36 @@
+/*Реализуйте функцию getline, которая считывает поток ввода посимвольно, пока не достигнет конца потока или не встретит символ переноса строки ('\n'), и возвращает C-style
+строку с прочитанными символами.
+
+Обратите внимание, что так как размер ввода заранее неизвестен, то вам нужно будет перевыделять память в процессе чтения, если в потоке ввода оказалось больше
+символов, чем вы ожидали.
+
+Память, возвращенная из функции будет освобождена оператором delete[]. Символ переноса строки ('\n') добавлять в строку не нужно, но не забудьте, что в конце C-style
+строки должен быть завершающий нулевой символ.*/
+
+char *resize(const char *str, unsigned size, unsigned new_size)
+{
+  char *new_memory = new char[new_size];
+    for (int i = 0; i < new_size; i++)
+        new_memory[i] = str[i];
+    delete[] str;
+    return new_memory;
+}
+
+char *getline()
+{
+    int size = 1, index = 0;
+    char c;
+    char *memory = new char[size];
+    while (cin.get(c) && (c != '\n') && (c != EOF))
+    {
+        memory[index] = c;
+        index++;
+        if (index == size)
+        {
+            memory = resize(memory, size, size * 2);
+            size *= 2;
+        }
+    }
+    memory[index] = '\0';
+    return memory;
+}
