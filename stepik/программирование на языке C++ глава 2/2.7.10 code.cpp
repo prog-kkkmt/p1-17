8f4f233d8f109fbea4c8482133b44b09428d9926
Sergey <64083235+sootrd@users.noreply.github.com>
2020-05-12 14:44:41 +0300

    Add files via upload

---
+++ stepik/программирование на языке C++ глава 2/2.7.10 code.cpp
@@ -0,0 +1,20 @@
+
+void swap_min(int* m[], unsigned rows, unsigned cols)
+{
+	int min = m[0][0];
+	int str = 0;
+	for (int i = 0; i != rows; i++)
+		for (int j = 0; j != cols; j++)
+			if (m[i][j] < min)
+			{
+				min = m[i][j]; //минимальное зачение
+				str = i; 
+			}
+	if (str != 0) {
+		int* tmp = new int[cols]; //временный одномерный массив для нулевой строки
+		for (int i = 0; i != cols; i++)
+			tmp = m[0];
+		m[0] = m[str]; //свап строк местами
+		m[str] = tmp;
+	}
+}
