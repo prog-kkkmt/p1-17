4fd25d0b2c49575c51f2216eec70c876fa185970
OVOSKOP <parodia1234321@gmail.com>
2020-05-16 13:44:58 +0300

    regexp 5

--- Регулярки/regexp.cpp
+++ Регулярки/regexp.cpp
@@ -1,3 +1,8 @@
+// 	С++ regexp test	
+//	Developer: Pishchulin Igor 
+// 	16.05.2020
+
+
 #include <iostream>
 #include <string>
 #include <regex>
@@ -14,11 +19,11 @@ int main()
     cout << "\nEnter regexp: ";
     cin >> reg;
 
-    regex re(reg);
+    regex re(reg); //create of regexp object
  
     cout << "\nSearching for " << reg << " in " << str << ":";
 	smatch m;
-    regex_search(str, m, re);
+    regex_search(str, m, re); //searching for the first entry
     cout << "\n1) " << m[0] << '\n';
 
     return 0;
--- Регулярки/regexp.py
+++ Регулярки/regexp.py
@@ -1,10 +1,15 @@
-import re
+## 	Python regexp test	
+##	Developer: Pishchulin Igor 
+## 	16.05.2020
+##
+
+import re # подключаем библиотеку
 
 stra = input("Введите строку для поиска: ")
 reg = input("\nВведите регулярное выражение(без //, в формате [asd]+): ")
 
-regexp = re.compile(reg)
+regexp = re.compile(reg) # создаем объект выражение
 
-match = regexp.match(stra)
+match = regexp.match(stra) #ищем соответствие c нулевой позиции
 
 print("\nПервое совпадение: '" + match.group(0) + "' в позициях {" + str(match.start(0)) + ', ' + str(match.end(0)) + "}")
