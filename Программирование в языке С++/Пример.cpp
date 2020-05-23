5c0463c5c8b6594d3f0f8b3dd22ebc9b1bd2a7df
Valeriya01 <64081708+Valeriya01@users.noreply.github.com>
2020-05-23 18:19:44 +0300

    Add files via upload

---
+++ Программирование в языке С++/Пример.cpp
@@ -0,0 +1,29 @@
+///Выполнила:Суркова Валерия
+
+#include <stdio.h>
+
+class CCircle
+{
+private:
+    int x, y;
+    const int R;
+public:
+    CCircle(int _x = 0, int _y = 0, int _r = 1);
+    void prn();
+};
+CCircle::CCircle(int _x, int _y, int _r) : R(_r)
+{
+    x = _x;
+    y = _y;
+}
+void CCircle::prn()
+{
+    printf("Center (%d, %d), R = %d.\n", x, y, R);
+}
+int main()
+{
+    CCircle x(7, 8, 9), y;
+    x.prn();
+    y.prn();
+    return 0;
+}
