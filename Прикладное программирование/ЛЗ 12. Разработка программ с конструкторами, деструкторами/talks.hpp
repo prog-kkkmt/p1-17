9c55c5bb231942bd2eb40962b9a09cacb3bce01d
Lika913 <58809903+Lika913@users.noreply.github.com>
2020-05-29 12:02:17 +0300

    Add files via upload

---
+++ Прикладное программирование/ЛЗ 12. Разработка программ с конструкторами, деструкторами/talks.hpp
@@ -0,0 +1,33 @@
+#ifndef TALKS_HPP_INCLUDED
+#define TALKS_HPP_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Talks//Города
+{
+	Talks();
+	
+	int codeT;
+	int codeC;
+	char* start;
+	size_t start_len;
+	int lasting; 
+	~Talks();
+
+};
+
+void input(vector <Talks>& v, Talks talks);
+void output(vector <Talks> v);
+void update(vector <Talks>& v); 
+void add(vector <Talks>& v, Talks talks);
+void del(vector <Talks>& v);
+void edit(vector <Talks>& v);
+void sorting(vector <Talks>& v);
+void aboutprogram();
+void functTalks();
+#endif 
