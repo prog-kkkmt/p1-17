210fa717190d34a485d7ab8808d15d2a4e400abb
nozmek <64076485+nozmek@users.noreply.github.com>
2020-05-29 13:39:15 +0300

    Update city.hpp

--- прикладное программирование/ЛЗ 10/city.hpp
+++ прикладное программирование/ЛЗ 10/city.hpp
@@ -15,13 +15,13 @@ struct City//Города
 	int codeR; //код тарифа
 };
 
-void input(vector <City>& v, City city);
-void output(vector <City> v);
-void update(vector <City>& v); 
-void add(vector <City>& v, City city);
-void del(vector <City>& v);
-void edit(vector <City>& v);
-void aboutProgram();
-void sorting(vector <City>& v);
+void input(vector <City>& v, City city);//чтение из файла
+void output(vector <City> v); //вывод
+void update(vector <City>& v); //обновление
+void add(vector <City>& v, City city);//добавление
+void del(vector <City>& v);//удаление
+void edit(vector <City>& v);//редактирование
+void aboutProgram();//о программе
+void sorting(vector <City>& v);//сортировка 
 
 #endif 
