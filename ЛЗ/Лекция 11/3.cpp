3fb5211b472718d26cca6b61b39b1e941a31364b
kek4jk <64089144+kek4jk@users.noreply.github.com>
2020-05-29 15:46:58 +0300

    Create 3.cpp

---
+++ ЛЗ/Лекция 11/3.cpp
@@ -0,0 +1,39 @@
+# include <stdio.h>
+class CPoint
+{
+    private:
+        int *px;                                            
+        int *py;
+    public:
+        CPoint(int x = 0, int _y = 0);
+        ~CPoint();                                        
+        void Prn();         
+};
+
+CPoint::CPoint(int _x, int _y)
+{
+    px = new int;                                   
+    *px = _x;
+    py = new int;                        
+    *py = _y;
+}
+
+CPoint::~CPoint()
+{
+    delete px;                             
+    delete py;                  
+}
+  
+void CPoint::Prn()
+{
+    printf("Tochka (%d, %d);\n", *px, *py);
+}
+   
+int main()
+{
+    CPoint A(3,9), B;
+    A.Prn();
+    B.Prn();
+
+    return 0;
+}
