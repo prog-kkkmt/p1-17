5009e1c31932aa58302893cc38e682f015dc800a
Lika913 <58809903+Lika913@users.noreply.github.com>
2020-05-29 12:19:09 +0300

    Add files via upload

---
+++ Прикладное программирование/ЛЗ 19. Построение HTML-страниц/main.cpp
@@ -0,0 +1,50 @@
+/**
+Междугородная телефонная станция.
+В базе хранится информация о продолжительности и стоимости междугородных звонков по России.
+Таблицы:
+Города (Код города, расстояние, Код тарифа)
+Тариф (Код тарифа, минимальное расстояние, максимальное расстояние, цена);
+Разговоры (Код разговора, Код города, начало, продолжительность).
+Доработать программу из лабораторного занятия 10.
+Добавить пункты меню для ввода, редактирования, удаления и печати всех таблиц задания.
+**/
+
+#include "talks.hpp"
+#include "rate.hpp"
+#include "city.hpp"
+
+int main()
+{
+	int number;
+	bool ok = true;
+	cout << "1 - Rate \n2 - Talks \n3 - City \n4 - about program \n0 - exit\n";
+	while(ok)
+	{
+		cout << "number of table : ";
+		cin >> number;
+		switch(number)
+		{
+			case 1:
+			functRate();
+			break;
+			case 2:
+			functTalks();
+			break;
+			case 3:
+			functCity();
+			break;
+			case 4:
+			aboutProgram();
+			break;
+			case 0:
+			ok = false;
+			break;
+			default:
+			cout << "incorrect number!" << endl;
+			break;
+		};
+	}
+	return 0;
+}
+
+///Выполнила: Фролова Л.А.
---
+++ Прикладное программирование/ЛЗ 19. Построение HTML-страниц/talks.cpp
@@ -0,0 +1,44 @@
+//Таблица Разговоры (Код разговора, Код города, начало, продолжительность).
+#include "talks.hpp"
+
+void functTalks()
+{
+
+	vector <Talks> v;
+	Talks talks;
+	int number;
+	bool ok = true;
+
+	input(v, talks);
+	to_html(v);
+
+	while(ok)
+	{
+		cout << "number : ";
+		cin >> number;
+		switch(number)
+		{
+			case 1:
+			add(v, talks);
+			break;
+			case 2:
+			del(v);
+			break;
+			case 3:
+			edit(v);
+			break;
+			case 4:
+			output(v);
+			break;
+			case 5:
+			sorting(v);
+			break;
+			case 0:
+			ok = false;
+			break;
+			default:
+			cout << "incorrect number!" << endl;
+			break;
+		};
+	}
+}
