ee4e6975d8d7bdb45616875081d4d2dd45e34c27
asferd-gm <64076162+asferd-gm@users.noreply.github.com>
2020-06-05 23:02:12 +0300

    Add files via upload

---
+++ ЛЗ21/RSA.py
@@ -0,0 +1,48 @@
+import random
+import sys
+import os
+import rabinMiller
+import cryptomath
+def main():
+    makeKeyFiles('RSA_demo', 1024)
+def generateKey(keySize):
+    print('Generating p prime...')
+    p = rabinMiller.generateLargePrime(keySize)
+    print('Generating q prime...')
+    q = rabinMiller.generateLargePrime(keySize)
+    n = p * q
+    print('Generating e that is relatively prime to (p-1)*(q-1)...')
+    while True:
+        e = random.randrange(2 ** (keySize - 1), 2 ** (keySize))
+        if cryptomath.gcd(e, (p - 1) * (q - 1)) == 1:
+            break
+    print('Calculating d that is mod inverse of e...')
+    d = cryptomath.findModInverse(e, (p - 1) * (q - 1))
+    publicKey = (n, e)
+    privateKey = (n, d)
+    print('Public key:', publicKey)
+    print('Private key:', privateKey)
+    return (publicKey, privateKey)
+
+def makeKeyFiles(name, keySize): 
+    if os.path.exists('%s_pubkey.txt' % (name)) or os.path.exists('%s_privkey.txt' % (name)):
+        sys.exit('WARNING: The file %s_pubkey.txt or %s_privkey.txt already exists! Use a different name or delete these files and re-run this program.' % (name, name))
+    publicKey, privateKey = generateKey(keySize)
+    print()
+    print('The public key is a %s and a %s digit number.' % (len(str(publicKey[0])), len(str(publicKey[1])))) 
+    print('Writing public key to file %s_pubkey.txt...' % (name))
+    fo = open('%s_pubkey.txt' % (name), 'w')
+    fo.write('%s,%s,%s' % (keySize, publicKey[0], publicKey[1]))
+    fo.close()
+    print()
+    print('The private key is a %s and a %s digit number.' % (len(str(publicKey[0])), len(str(publicKey[1]))))
+    print('Writing private key to file %s_privkey.txt...' % (name))
+    fo = open('%s_privkey.txt' % (name), 'w')
+    fo.write('%s,%s,%s' % (keySize, privateKey[0], privateKey[1]))
+    fo.close()
+
+
+
+if __name__ == '__main__':
+
+   main()
