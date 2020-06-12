34f153691ba658fb7172a255a5e29994da2820b8
bulatka2131488121 <61374383+bulatka2131488121@users.noreply.github.com>
2020-06-12 15:27:33 +0300

    Add files via upload

---
+++ Курсовая/bot.py
@@ -0,0 +1,56 @@
+import keybord as kb
+import config
+import logging
+import asyncio
+import pyowm
+import geo
+import json
+import telebot
+from datetime import datetime
+
+from aiogram import Bot, Dispatcher, executor, types
+
+import aiogram.types.base
+
+from sqlighter import SQLighter
+
+#
+# задаем уровень логов
+logging.basicConfig(level=logging.INFO)
+
+# инициализируем бота
+bot = Bot(token=config.API_TOKEN)
+owm = pyowm.OWM ('cc16c267326fd725e6c6dc2025143c34')
+dp = Dispatcher(bot)
+
+# инициализируем соединение с БД
+db = SQLighter('db.db')
+
+# Команда активации подписки
+@dp.message_handler(commands=['subscribe'])
+async def subscribe(message: types.Message):
+	if(not db.subscriber_exists(message.from_user.id)):
+		db.add_subscriber(message.from_user.id)
+	else:
+		db.update_subscription(message.from_user.id, True)
+	
+	await message.answer("А вы подписались на Raid Shadow Legends?????????????????????????????????????????")
+
+# Команда отписки
+@dp.message_handler(commands=['unsubscribe'])
+async def unsubscribe(message: types.Message):
+	if(not db.subscriber_exists(message.from_user.id)):
+		db.add_subscriber(message.from_user.id, False)
+		await message.answer("Вы итак не подписаны.")
+	else:
+		db.update_subscription(message.from_user.id, False)
+		await message.answer("Вы отписаны от рассылки.")
+
+#кретсики нолики
+@dp.message_handler(commands=['krest'])
+async def process_start_command(message: types.Message):
+    await message.answer("cocи", reply_markup=kb.inline_kb1)
+
+# запускаем лонг поллинг
+if __name__ == '__main__':
+	executor.start_polling(dp, skip_updates=True)
