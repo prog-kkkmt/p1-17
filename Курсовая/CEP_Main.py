4a88c92c6966bca4865c6285b65cfc5360a3a421
Pawel-Owtzhinnikow <64088500+Pawel-Owtzhinnikow@users.noreply.github.com>
2020-06-20 14:35:23 +0300

    Add files via upload

---
+++ Курсовая/CEP_Main.py
@@ -0,0 +1,25 @@
+import sys
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from CEP_Main_Graphs import *
+
+
+class MainWin(QtWidgets.QMainWindow):
+
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+    def mbox(self, body, title='Error'):
+        dialog = QMessageBox(QMessageBox.Information, title, body)
+        dialog.exec_()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    application = MainWin()
+    application.show()
+    sys.exit(app.exec_())
