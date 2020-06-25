34fa1bb3f8f724e396b017b45b20c73196c3f5d4
bulatka2131488121 <61374383+bulatka2131488121@users.noreply.github.com>
2020-06-25 12:42:20 +0300

    Add files via upload

---
+++ Курсовая/bot.py
@@ -0,0 +1,69 @@
+import keybord as kb
+import logging
+import asyncio
+import pyowm
+import geo
+import aiogram.types.base
+
+from aiogram import Bot, Dispatcher, executor, types
+
+from sqlighter import SQLighter
+
+# задаем уровень логов
+logging.basicConfig(level=logging.INFO)
+
+# инициализируем бота
+bot = Bot(token='1296584769:AAEu7y4Qjz9y7k6zA0rn8Tz9PmzUnJA4mB0')
+
+dp = Dispatcher(bot)
+
+# инициализируем соединение с БД
+db = SQLighter('db.db')
+
+# Команда активации подписки
+@dp.message_handler(commands=['subscribe'])
+async def subscribe(message: types.Message):
+	if(not db.subscriber_exists(message.from_user.id)):
+		db.add_subscriber(message.from_user.id)
+	else:
+		db.update_subscription(message.from_user.id, True)
+	
+	await message.answer("Вы подписались на бота")
+
+# Команда отписки
+@dp.message_handler(commands=['unsubscribe'])
+async def unsubscribe(message: types.Message):
+	if(not db.subscriber_exists(message.from_user.id)):
+		db.add_subscriber(message.from_user.id, False)
+		await message.answer("Вы итак не подписаны.")
+	else:
+		db.update_subscription(message.from_user.id, False)
+		await message.answer("Вы отписаны от рассылки.")
+
+#погода
+@dp.message_handler(commands = ['weather'])
+async def weather(message: types.Message):
+          await message.answer("Введите город:")
+          @dp.message_handler(content_types=["text"])
+          async def get_city(message):
+                  city = message.text # сохраняем данные
+                  await message.answer(geo.weatherd(city))
+          
+#кретсики нолики
+@dp.message_handler(commands=['krest'])
+async def krest(message: types.Message):
+    await message.answer('krest', reply_markup=kb.inline_kb1)
+
+#кнопки
+@dp.message_handler(commands=['start'])
+async def buttons(message: types.Message):
+    await message.answer('btn', reply_markup=kb.markup3)
+
+#удаление кнопок
+@dp.message_handler(commands=['remove'])
+async def removekb(message: types.Message):
+    await message.answer('remove_kb', reply_markup=kb.ReplyKeyboardRemove())
+
+# запускаем лонг поллинг
+if __name__ == '__main__':
+	executor.start_polling(dp, skip_updates=True)
---
+++ Курсовая/sqlighter.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+class SQLighter:
+
+    def __init__(self, database):
+        """Подключаемся к БД и сохраняем курсор соединения"""
+        self.connection = sqlite3.connect(database)
+        self.cursor = self.connection.cursor()
+
+    def get_subscriptions(self, status = True):
+        """Получаем всех активных подписчиков бота"""
+        with self.connection:
+            return self.cursor.execute("SELECT * FROM `subscriptions` WHERE `status` = ?", (status,)).fetchall()
+
+    def subscriber_exists(self, user_id):
+        """Проверяем, есть ли уже юзер в базе"""
+        with self.connection:
+            result = self.cursor.execute('SELECT * FROM `subscriptions` WHERE `user_id` = ?', (user_id,)).fetchall()
+            return bool(len(result))
+
+    def add_subscriber(self, user_id, status = True):
+        """Добавляем нового подписчика"""
+        with self.connection:
+            return self.cursor.execute("INSERT INTO `subscriptions` (`user_id`, `status`) VALUES(?,?)", (user_id,status))
+
+    def update_subscription(self, user_id, status):
+        """Обновляем статус подписки пользователя"""
+        with self.connection:
+            return self.cursor.execute("UPDATE `subscriptions` SET `status` = ? WHERE `user_id` = ?", (status, user_id))
+
+    def close(self):
+        """Закрываем соединение с БД"""
+        self.connection.close()
