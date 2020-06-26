dbebed94319909f6cdf2510c930863ea77ff06ce
Valeriya01 <64081708+Valeriya01@users.noreply.github.com>
2020-06-26 15:31:54 +0300

    Update marshryt.py

--- Курсовая работа/код/marshryt.py
+++ Курсовая работа/код/marshryt.py
@@ -1,10 +1,12 @@
+##Этот модуль запускает окно с информацией о маршрутах
+
 from tkinter import *
 from tkinter import ttk
 import ctypes
 import tkinter as tk
 import random
 
-def marsh():
+def marsh(): ## функция, открывающая окно с информацией о маршрутах
     window = Tk()
     window.resizable(False, False)
     window.title("Маршруты")
@@ -18,25 +20,25 @@ def marsh():
     canvas.configure(bg = 'pink')
     canvas.pack()
 
-    f = open('marsh.txt', 'r', encoding = 'utf-8')
+    f = open('marsh.txt', 'r', encoding = 'utf-8') ## открытие и чтение файла
     text = Text()
     text = scrolledtext.ScrolledText(window, height = '17', font = 'Batang 20', width = '40')
     text.insert(INSERT, f.read())
     text.place(x = 425, y = 80)
     f.close()
 
-    def clicked_dobavlenie():
+    def clicked_dobavlenie(): ## функция добавления информации
         z = open('marsh.txt', 'r', encoding = 'utf-8')
         y = z.read()
         x = y.split()
         marsh = []
-        for i in range (0, len(x), 2):
+        for i in range (0, len(x), 2): 
             u = x[i] + ' ' + x[i+1]
             marsh.append(u)
         b = txt1.get()
         
 
-        f = open('marsh.txt', 'a', encoding = 'utf-8')
+        f = open('marsh.txt', 'a', encoding = 'utf-8') ## запись в файл
         u11 = txt1.get() + ' ' + com.get() + '\n'
         f.write(u11)
         f.close()
@@ -50,7 +52,7 @@ def marsh():
         f.close()
 
         
-    def clicked_delete():      
+    def clicked_delete(): ## функция удаления информации     
         z = open('marsh.txt', 'r', encoding = 'utf-8')
         y = z.read()
         x = y.split()
@@ -78,28 +80,28 @@ def marsh():
         f.close()
     
     
-    lbl1 = Label(window)
+    lbl1 = Label(window) ## метка
     lbl2 = Label(window)
     lbl1.configure(font = 'Batang 20', text = '  Дата', bg = 'pink', fg = 'crimson')
-    lbl1.place(x = 40, y = 81)
+    lbl1.place(x = 40, y = 81) ## положение метки 
     lbl2.configure(font = 'Batang 20', text = '  Направление', bg = 'pink', fg = 'crimson')
     lbl2.place(x = 170, y = 80)
-    txt1 = Entry(window, width = 10, font = 'Batang 20')  
+    txt1 = Entry(window, width = 10, font = 'Batang 20') ## поле ввода 
     txt1.place(x = 20, y = 120)
-    btn = Button(window, text = "Добавить", font = 'Batang 20', command = clicked_dobavlenie, bg = 'hotpink')  
+    btn = Button(window, text = "Добавить", font = 'Batang 20', command = clicked_dobavlenie, bg = 'hotpink') ## кнопка добавления
     btn.place(x = 105, y = 170)
     
-    lbl3 = Label(window)
+    lbl3 = Label(window) ## метка
     lbl4 = Label(window)
     lbl3.configure(font = 'Batang 20', text = '  Дата', bg = 'pink', fg = 'crimson')
-    lbl3.place(x = 40, y = 281)
+    lbl3.place(x = 40, y = 281) ## расположение метки 
     lbl4.configure(font = 'Batang 20', text = '  Направление', bg = 'pink', fg = 'crimson')
     lbl4.place(x = 170, y = 280)
     txt3 = Entry(window, width = 10, font = 'Batang 20')  
     txt3.place(x = 20, y = 320)
-    btn1 = Button(window, text = "Удалить", font = 'Batang 20', command = clicked_delete, bg = 'hotpink')  
+    btn1 = Button(window, text = "Удалить", font = 'Batang 20', command = clicked_delete, bg = 'hotpink') ## кнопка удаления 
     btn1.place(x = 105, y = 370)
-    com = ttk.Combobox(window)
+    com = ttk.Combobox(window) ## выпадающий список 
     com.place(x = 190, y = 120, height = 37, width = 155)
     com['values'] = ['Байкал', 'Тайга', 'Горный калейдоскоп', 'Плато Путорана', 'Камчатка', 'Восхождения на Эльбрус', 'Каньон реки Лава', 'Восхождение на вулкан Ключевая сопка', 'Велопоход вдоль Оки', 'Вокруг Эльбруса за 9 дней']
     com.current(0)
