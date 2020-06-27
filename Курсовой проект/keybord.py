5a06492761c9e87f05bd294ab3c4efcbf78c73d5
kek4jk <64089144+kek4jk@users.noreply.github.com>
2020-06-27 11:22:33 +0300

    Update keybord.py

--- Курсовой проект/keybord.py
+++ Курсовой проект/keybord.py
@@ -1,5 +1,7 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton, ReplyKeyboardMarkup
+#импорт модулей для работы с клавиатурой и кнопками
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton, ReplyKeyboardMarkup 
 
+#кнопки для меню
 button1 = InlineKeyboardButton(" ", callback_data='btn1')
 button2 = InlineKeyboardButton(" ", callback_data='btn2')
 button3 = InlineKeyboardButton(" ", callback_data='btn3')
@@ -10,18 +12,16 @@ button7 = InlineKeyboardButton(" ", callback_data='btn7')
 button8 = InlineKeyboardButton(" ", callback_data='btn8')
 button9 = InlineKeyboardButton(" ", callback_data='btn9')
 
-button10 = KeyboardButton('/weather')
-button11 = KeyboardButton('/subscribe')
-button12 = KeyboardButton('/unsubscribe')
-
-markup3 = ReplyKeyboardMarkup().row(
-    button10, button11, button12
-)
-
-
 inline_kb1 = InlineKeyboardMarkup()
-
+#печать в чат поля 3*3 из кнопок для будущего меню
 inline_kb1.add(
 	button1, button2, button3, button4, button5, button6, button7, button8, button9
 )
-
+#кнопки для виртуальной клавиатуры
+button10 = KeyboardButton('/weather')
+button11 = KeyboardButton('/subscribe')
+button12 = KeyboardButton('/unsubscribe')
+#вывод на экран виртуальной клавиатуры с кнопками
+markup3 = ReplyKeyboardMarkup().row(
+    button10, button11, button12
+)
