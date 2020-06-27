1c7459827b5876053a47bf95a13775ccf58957b9
kek4jk <64089144+kek4jk@users.noreply.github.com>
2020-06-27 11:21:37 +0300

    Update bot.py

--- Курсовой проект/bot.py
+++ Курсовой проект/bot.py
@@ -1,3 +1,4 @@
+#импорт модулей для работы бота 
 import keybord as kb
 import logging
 import asyncio
@@ -5,62 +6,62 @@ import pyowm
 import geo
 import aiogram.types.base
 
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor, types #импорт модулей для работы с TelegramBotAPI
 
-from sqlighter import SQLighter
+from sqlighter import SQLighter #импорт класса для работы с БД
 
-# задаем уровень логов
+#задаем уровень логов
 logging.basicConfig(level=logging.INFO)
 
-# инициализируем бота
+#инициализация бота
 bot = Bot(token='1296584769:AAEu7y4Qjz9y7k6zA0rn8Tz9PmzUnJA4mB0')
 
 dp = Dispatcher(bot)
 
-# инициализируем соединение с БД
+# инициализация соединения с БД
 db = SQLighter('db.db')
 
-# Команда активации подписки
-@dp.message_handler(commands=['subscribe'])
+#команда активации подписки
+@dp.message_handler(commands=['subscribe'])#если сообщение от пользователя 'subscribe' делать дальнейшие команды
+
+#async def — определяет native coroutine function, результатом вызова которой будет объект-сопрограмма native coroutine, пока еще не запущенная.
 async def subscribe(message: types.Message):
+
+    #проверка на наличие подписки у пользователя
 	if(not db.subscriber_exists(message.from_user.id)):
 		db.add_subscriber(message.from_user.id)
 	else:
 		db.update_subscription(message.from_user.id, True)
-	
-	await message.answer("Вы подписались на бота")
 
-# Команда отписки
-@dp.message_handler(commands=['unsubscribe'])
-async def unsubscribe(message: types.Message):
+#async def —  Сопрограмма  (coroutine) асинхронная функция 
+async def subscribe(message: types.Message):
+
+    #проверка на наличие подписки у пользователя
 	if(not db.subscriber_exists(message.from_user.id)):
-		db.add_subscriber(message.from_user.id, False)
-		await message.answer("Вы итак не подписаны.")
+		db.add_subscriber(message.from_user.id)
 	else:
-		db.update_subscription(message.from_user.id, False)
-		await message.answer("Вы отписаны от рассылки.")
-
+		db.update_subscription(message.from_user.id, True)
+		'''Ключевое слово await приостанавливает выполнение текущей сопрограммы (coroutine) и вызывает  message.answer.'''
+		await message.answer("Вы подписались на бота")
 #погода
 @dp.message_handler(commands = ['weather'])
 async def weather(message: types.Message):
           await message.answer("Введите город:")
           @dp.message_handler(content_types=["text"])
           async def get_city(message):
-                  city = message.text # сохраняем данные
-                  await message.answer(geo.weatherd(city))
-          
- 
+                  city = message.text # сохраняем данные о городе
+                  await message.answer(geo.weatherd(city)) # печать в чат погоды    
 
-#кнопки
+#вывод кнопки команд на экран
 @dp.message_handler(commands=['start'])
 async def buttons(message: types.Message):
     await message.answer('btn', reply_markup=kb.markup3)
 
-#удаление кнопок
+#скрытие с экрана кнопок
 @dp.message_handler(commands=['remove'])
 async def removekb(message: types.Message):
     await message.answer('remove_kb', reply_markup=kb.ReplyKeyboardRemove())
 
-# запускаем лонг поллинг
+#запуск бота
 if __name__ == '__main__':
 	executor.start_polling(dp, skip_updates=True)
