ebd5bf079fad2f126ec198282ef495cfb246ee87
kek4jk <64089144+kek4jk@users.noreply.github.com>
2020-06-27 14:04:59 +0300

    Update bot.py

--- Курсовой проект/bot.py
+++ Курсовой проект/bot.py
@@ -1,3 +1,4 @@
+# Булатников Илья П1-17
 #импорт модулей для работы бота 
 import keybord as kb
 import logging
@@ -6,7 +7,8 @@ import pyowm
 import geo
 import aiogram.types.base
 
-from aiogram import Bot, Dispatcher, executor, types #импорт модулей для работы с TelegramBotAPI
+from aiogram import Bot, Dispatcher, executor, types
+#импорт модулей для работы с TelegramBotAPI
 
 from sqlighter import SQLighter #импорт класса для работы с БД
 
@@ -22,16 +24,8 @@ dp = Dispatcher(bot)
 db = SQLighter('db.db')
 
 #команда активации подписки
-@dp.message_handler(commands=['subscribe'])#если сообщение от пользователя 'subscribe' делать дальнейшие команды
-
-#async def — определяет native coroutine function, результатом вызова которой будет объект-сопрограмма native coroutine, пока еще не запущенная.
-async def subscribe(message: types.Message):
-
-    #проверка на наличие подписки у пользователя
-	if(not db.subscriber_exists(message.from_user.id)):
-		db.add_subscriber(message.from_user.id)
-	else:
-		db.update_subscription(message.from_user.id, True)
+@dp.message_handler(commands=['subscribe'])
+#если сообщение от пользователя 'subscribe' делать дальнейшие команды
 
 #async def —  Сопрограмма  (coroutine) асинхронная функция 
 async def subscribe(message: types.Message):
@@ -41,7 +35,16 @@ async def subscribe(message: types.Message):
 		db.add_subscriber(message.from_user.id)
 	else:
 		db.update_subscription(message.from_user.id, True)
-		'''Ключевое слово await приостанавливает выполнение текущей сопрограммы (coroutine) и вызывает  message.answer.'''
+
+async def subscribe(message: types.Message):
+
+    #проверка на наличие подписки у пользователя
+	if(not db.subscriber_exists(message.from_user.id)):
+		db.add_subscriber(message.from_user.id)
+	else:
+		db.update_subscription(message.from_user.id, True)
+		#Ключевое слово await приостанавливает выполнение текущей
+        #сопрограммы (coroutine) и вызывает  message.answer.
 		await message.answer("Вы подписались на бота")
 #погода
 @dp.message_handler(commands = ['weather'])
@@ -49,8 +52,10 @@ async def weather(message: types.Message):
           await message.answer("Введите город:")
           @dp.message_handler(content_types=["text"])
           async def get_city(message):
-                  city = message.text # сохраняем данные о городе
-                  await message.answer(geo.weatherd(city)) # печать в чат погоды    
+                  # сохраняем данные о городе
+                  city = message.text 
+                  # печать в чат погоды
+                  await message.answer(geo.weatherd(city))     
 
 #вывод кнопки команд на экран
 @dp.message_handler(commands=['start'])
