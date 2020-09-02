804c87c6fff6a2c986c68a6087344ec6a1e2c7b4
Ibra18 <64108578+Ibra18@users.noreply.github.com>
2020-09-02 15:12:57 +0300

    Add files via upload

---
+++ Прикладное программирование/ЛЗ2/1_8_5.cpp
@@ -0,0 +1,25 @@
+/*Sample Input 1:
+
+1 4 4
+Sample Output 1:
+
+-2 -2
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    double d = b * b - 4 * a * c;
+    if (d < 0)
+        cout << "No real roots";
+    else
+        cout << ((-1.0 * b) + sqrt(d)) / (2 * a) << " " << ((-1.0 * b) - sqrt(d)) / (2 * a);
+    return 0;
+}
