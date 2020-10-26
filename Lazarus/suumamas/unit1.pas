9314c19fb0e734ec5623cc4a68511bceef1f6033
Ibra18 <64108578+Ibra18@users.noreply.github.com>
2020-10-26 15:38:50 +0300

    Add files via upload

---
+++ Lazarus/suumamas/unit1.pas
@@ -0,0 +1,52 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Grids;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    StringGrid1: TStringGrid;
+    StringGrid2: TStringGrid;
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+   i, j, sum: Integer;
+begin
+     sum := 0;
+     for i := 0 to 4 do begin
+         for j := 0 to 4 do begin //цикл на ввод двумерного массива
+             StringGrid1.Cells[i, j] := IntToStr(Random(100));//присвоение элемнтам массива рандомным значениям
+             sum := sum + StrToInt(StringGrid1.Cells[i, j]); //подсчет суммы элементов столбца
+             StringGrid2.Cells[i, 0] := IntToStr(sum); //вывод суммы
+         end;
+         sum := 0;
+     end;
+
+
+   end;
+
+end.
+
+
